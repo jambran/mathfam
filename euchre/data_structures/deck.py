507ce29aa521289331479972fc27669a1b3dbc56
Jamie Brandon <jamiebrandon921@gmail.com>
2020-09-11 20:18:43 -0400

    deck's discussion points listed

---
+++ euchre/data_structures/deck.py
@@ -0,0 +1,11 @@
+class Deck:
+    """
+    - Features
+		○ Cards - dictionary (mapping (index of shuffle to Card))?
+	- Functions
+		○ Add_card(card)
+		○ Remove_card()
+		○ Shuffle()
+			§ Generate list of random numbers (same size as num cards) - use these indices for the order
+		○ Refresh()?
+    """

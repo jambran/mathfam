287d165dbb06eb996c8dcca1283fbf4e02cb14f9
Jamie Brandon <jamiebrandon921@gmail.com>
2020-09-11 20:16:28 -0400

    card's discussion points listed

--- euchre/data_structures/card.py
+++ euchre/data_structures/card.py
@@ -1,3 +1,13 @@
 class Card:
+
+    """
+    - features
+		○ Suit
+		○ Value
+	- Functions
+		○ Get_color
+		○ Is_playable(other_card) - do we need this? I think so
+		○ Compare_whos_best(card)
+    """
     def __init__(self):
-        raise No
+        raise NotImplementedError

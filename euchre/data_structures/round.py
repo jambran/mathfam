805aa5a81f37d8b52515118ddce3697544fe7dfd
Jamie Brandon <jamiebrandon921@gmail.com>
2020-10-08 16:29:10 -0400

    need to start building out this class to play rounds

--- euchre/data_structures/round.py
+++ euchre/data_structures/round.py
@@ -1,5 +1,15 @@
+from .player import Player
+from typing import List
+
+
 class Round:
     """
     - Features
         Keep track of the points
     """
+
+    def __init__(self, players: List[Player]):
+        self.players = players
+
+    def play(self):
+        return None
